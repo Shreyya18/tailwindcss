3dca71743bde8b39a0bc60dae2e9c2aa2ae7669e
shreyya18 <shreyapoojary251@gmail.com>
2025-02-16 14:04:21 +0530

    inital update

---
+++ my-tailwind-projecct/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div className='bg-deepblue container'>
+      <h1 className="text-5xl font-bold text-white text-center ">This is tailwindccs v4.0</h1>
+      <button className='bg-amber text-albaster text-3xl font-bold mt-12 rounded px-6 py-4 border-4 animate-pulse '>Click here</button>
+
+    <input type="text" placeholder='Enter your name'className='bg-white text-lg text-amber border-2 p-4 rounded ml-4 outline-0 '/>
+    </div>
+    <div className="@container border p-4 ">
+      <div className='grid grid-cols-1 @sm:grid-cols-2 @lg:grid-cols-4 gap-4'>
+        <div className='bg-blue-400 h-40'></div>
+        <div className='bg-green-500 h-40'></div>
+        <div className='bg-red-500 h-40'></div>
+        <div className='bg-yellow-500 h-40'></div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
