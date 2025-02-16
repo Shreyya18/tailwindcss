b0f8f6a9af71fe0735a52bf13867a131acd839e3
shreyya18 <shreyapoojary251@gmail.com>
2025-02-16 21:06:41 +0530

    inset utility

--- my-tailwind-projecct/src/App.jsx
+++ my-tailwind-projecct/src/App.jsx
@@ -28,7 +28,13 @@ const App = () => {
     <div className='bg-radial from-yellow-500 to-red-500 size-64 rounded-full'></div>
     </div>
 
-    
+    <div className=' grid gap-4 place-items-center bg-deepblue'>
+      <input type="text" placeholder='enter your name' className='inset-shadow-sm inset-shadow-amber-500 border-4 rounded-sm w-96 h-18 p-4 text-xl text-amber outline-none ' />
+
+      <input type="text" placeholder='enter your name' className='inset-shadow-sm inset-shadow-amber-500 ring-4 rounded-sm w-96 h-18 p-4 text-xl text-amber outline-none ' />
+
+      <input type="text" placeholder='enter your name' className='inset-ring-4 inset-shadow-amber-500 ring-4 rounded-sm w-96 h-18 p-4 text-xl text-amber outline-none ' />
+    </div>
     </>
   )
 }
