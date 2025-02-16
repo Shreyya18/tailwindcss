f932cef87dbc972f4ed0b1260c008ad571ccdde9
shreyya18 <shreyapoojary251@gmail.com>
2025-02-16 14:41:57 +0530

    update

--- my-tailwind-projecct/src/App.jsx
+++ my-tailwind-projecct/src/App.jsx
@@ -10,11 +10,10 @@ const App = () => {
     <input type="text" placeholder='Enter your name'className='bg-white text-lg text-amber border-2 p-4 rounded ml-4 outline-0 '/>
     </div>
     <div className="@container border p-4 ">
-      <div className='grid grid-cols-1 @sm:grid-cols-2 @lg:grid-cols-4 gap-4'>
-        <div className='bg-blue-400 h-40'></div>
-        <div className='bg-green-500 h-40'></div>
-        <div className='bg-red-500 h-40'></div>
-        <div className='bg-yellow-500 h-40'></div>
+      <div className='flex gap-4'>
+        <div className='bg-blue-400 h-40 w-64 ' ></div>
+        <div className='bg-green-500 h-40 w-64 @min-md:@max-xl:hidden '></div>
+        <div className='bg-red-500 h-40 w-64 '></div>
       </div>
     </div>
     </>
